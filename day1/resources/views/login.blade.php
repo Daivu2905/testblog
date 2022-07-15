cb8425f0054c3870f73581e8dae3fac581f5647f
Daivu <daivu77777@gmail.com>
2022-07-15 14:27:03 +0700

    tinhnang

---
+++ day1/resources/views/login.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xtra Blog</title>
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <!-- https://fontawesome.com/ -->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <!-- https://fonts.google.com/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/templatemo-xtra-blog.css" rel="stylesheet">
+</head>
+
+<body>
+    <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Create Account</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="list">
+                    <span>or use your email for registration</span>
+                    <input type="text" placeholder="Hoten" />
+                    <input type="Sdt" placeholder="Sdt" />
+                    <input type="password" placeholder="Password" />
+                    <input type="Birthday" placeholder="Birthday" />
+                    <input type="Address" placeholder="Address" />
+                    <input type="File" placeholder="File" />
+                    <input type="Work" placeholder="Work" />
+                    <input type="Description" placeholder="Description" />
+                    <button>Sign Up</button>
+                </div>
+
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/login.js"></script>
+</body>
+
+</html>
---
+++ day1/resources/views/used.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xtra Blog</title>
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <!-- https://fontawesome.com/ -->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <!-- https://fonts.google.com/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/templatemo-xtra-blog.css" rel="stylesheet">
+    <!--
+    
+TemplateMo 553 Xtra Blog
+
+https://templatemo.com/tm-553-xtra-blog
+
+-->
+</head>
+
+<body>
+    <div class="combo_list tm-header">
+        <div class="tm-header-wrapper">
+            <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="tm-site-header">
+                <h1 class="text-center">Xtra Blog</h1>
+                <a href="#">
+                    <img class="profile" src="https://viknews.com/wp-content/uploads/2021/12/Anh-Chill-buon-dep3-1.jpg" alt="anh dai dien">
+                </a>
+
+
+
+            </div>
+            <nav class="tm-nav" id="tm-nav">
+                <ul>
+                    <li class="tm-nav-item active">
+                        <a href="index.html" class="tm-nav-link">
+                            <i class="fas fa-home"></i> Blog Home
+                        </a>
+                    </li>
+                    <li class="tm-nav-item">
+                        <a href="post.html" class="tm-nav-link">
+                            <i class="fas fa-pen"></i> Single Post
+                        </a>
+                    </li>
+                    <li class="tm-nav-item">
+                        <a href="login.html" class="tm-nav-link">
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="information">
+        <table>
+            <tr>
+                <td><b>Họ Tên :</b></td>
+                <td>HOANG KHANH LINH</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td><b>Số điện thoại :</b></td>
+                <td>0965249555</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td><b>Email :</b></td>
+                <td>lili2222@gmail.com</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td><b>Birthday :</b></td>
+                <td>22/3/1992</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td><b>Address :</b></td>
+                <td>saddddddadasad</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td><b>Work :</b></td>
+                <td>IT</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+            <tr>
+                <td> <b>Description:</b></td>
+                <td>sadsadsadasdsadsafasfsaf</td>
+                <td><button type="button" class="btn btn-primary ">EDIT</button></td>
+                <td><button type="button" class="btn btn-danger">DELETE</button></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
